c3b7016ff458c28616f8ca2ac0429277116da27a
Pasindu Lakmal <nanayakk-cs18027@stu.kln.ac.lk>
2023-08-17 22:20:57 +0530

    projectCommit

---
+++ backend/backend/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ backend/backend/Models/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=empdb;User Id=pasindu;password=12345; TrustServerCertificate=True");
+        }
+    }
+}
